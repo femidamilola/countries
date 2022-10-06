46d507f6a08652a6067f8f625c39a2425109d01c
femidamilola <femi.adebayo1998@gmail.com>
2022-10-06 18:52:45 +0100

    GraphQl

--- graphql/types/Record.ts
+++ graphql/types/Record.ts
@@ -6,6 +6,7 @@ import {
   stringArg,
   nonNull,
   unionType,
+  nullable,
 } from "nexus";
 
 export const RecordCountry = objectType({
@@ -66,6 +67,103 @@ export const RecordsQuery = extendType({
       type: "Record",
       resolve(_parent, _args, ctx) {
         return ctx.prisma.record.findMany();
+      },
+    });
+  },
+});
+
+export const CreateRecordMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("createRecord", {
+      type: "Record",
+      args: {
+        countryId: nonNull(stringArg()),
+        year: nonNull(intArg()),
+        population: nonNull(intArg()),
+      },
+      async resolve(_, args, ctx) {
+        let queryResult;
+        queryResult = await ctx.prisma.record.create({
+          data: {
+            countryId: args.countryId,
+            year: args.year,
+            population: args.population,
+          },
+        });
+        return queryResult;
+      },
+    });
+  },
+});
+
+export const UpdateRecordMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("updateRecord", {
+      type: "Record",
+      args: {
+        id: nonNull(stringArg()),
+        countryId: nullable(stringArg()),
+        year: nullable(intArg()),
+        population: nullable(intArg()),
+      },
+      async resolve(
+        _,
+        args: {
+          countryId: string;
+          year: number;
+          population: number;
+          id: string;
+        },
+        ctx
+      ) {
+        let queryResult;
+        queryResult = await ctx.prisma.record.update({
+          where: {
+            id: args.id,
+          },
+          data: {
+            countryId: args.countryId,
+            year: args.year,
+            population: args.population,
+          },
+        });
+        return queryResult;
+      },
+    });
+  },
+});
+
+export const MutationMessage = objectType({
+  name: "MutationMessage",
+  definition(t) {
+    t.string("message");
+  },
+});
+
+export const DeleteRecordMutation = extendType({
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("deleteRecord", {
+      type: "Record",
+      args: {
+        id: nonNull(stringArg()),
+      },
+      async resolve(_, { id }, ctx) {
+        let queryResult;
+        queryResult = await ctx.prisma.record.delete({
+          where: {
+            id,
+          },
+        });
+        return { ...queryResult, message: "Deleted successfully" };
+      },
+    });
+    t.nonNull.field("message", {
+      type: "MutationMessage",
+      resolve() {
+        return { message: "Deleted Successfully" };
       },
     });
   },
